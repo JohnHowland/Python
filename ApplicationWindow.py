efdf6448a208adfbcfec3cb1412bf17f3fb65885
John Howland <chase.howland1@gmail.com>
2015-03-01 21:18:40 -0500

    List Option
    - Added the list window
    - Working on calculating the amount of time passed

--- ApplicationWindow.py
+++ ApplicationWindow.py
@@ -1,18 +1,21 @@
 from Tkinter import *
 from ttk import *
 import Database as db
+import string
 from time import strftime
 from PIL import Image, ImageTk
 
 itemFilepath = "C:\\Projects\\Testing Location\\Smart Kitchen\\Product Image\\00000000.gif"
 
+
+#############################  Main Application Window  ##################################
 class ApplicationWindow():
     def __init__(self, master):
         self.master = master
 ##        self.master.resizable(FALSE, FALSE)
         self.frame = Frame(self.master)
         self.master.bind('<Button-1>', self.click_button)
-        self.master.curIndex = None
+##        self.master.curIndex = None
         
 ##        self.textFrame = Frame(self.master)
         self.initUI()
@@ -32,7 +35,7 @@ class ApplicationWindow():
         self.lbl = Label(self.frame, text="Windows")
         self.lbl.grid(row=0, column=0, sticky=W, padx=5, pady=5)
 
-        self.area = Listbox(self.frame, selectmode=SINGLE)
+        self.area = Listbox(self.frame, selectmode=SINGLE, height=17, width=25)
         self.area.grid(row=1, column=0, rowspan=10, columnspan=2, padx=5, pady=5)
         
         self.itemName = Entry(self.frame, width = 18)
@@ -47,12 +50,12 @@ class ApplicationWindow():
 
         self.itemSelfLifeTime = Entry(self.frame, width = 15)
         self.itemSelfLifeTime.grid(row=4, column=4, padx=5, sticky=W)
-        self.itemSelfLifeTime_LABEL = Label(self.frame, text = "Date Expires")
+        self.itemSelfLifeTime_LABEL = Label(self.frame, text = "Expire Time")
         self.itemSelfLifeTime_LABEL.grid(row=3, column=4, pady=2, padx=5, sticky=W)
 
         itemFilepath = "C:\\Projects\\Testing Location\\Smart Kitchen\\Product Image\\NoPictureAvailable.gif"
         self.photo = PhotoImage(file = itemFilepath)
-        self.imageCanvas = Canvas(self.frame, width=220, height=220)
+        self.imageCanvas = Canvas(self.frame, width=200, height=200)
         self.imageCanvas.grid(row=5, column=3, rowspan=6, columnspan=3, padx=5, pady=5)
         self.imageCanvas.create_image(100,100, image = self.photo)
 
@@ -74,6 +77,11 @@ class ApplicationWindow():
 ##        self.obtn = Button(self.frame, text="OK", command = test)
 ##        self.obtn.grid(row=5, column=3)
 
+        self.ListButton = Button(self.frame, text="List", command = self.ShowList)
+        self.ListButton.grid(row=12, column=0, padx=5, pady = 5, sticky=W+S)
+
+#############################  Functions for Main Window  ##################################
+
     def InsertListItem(self, arg):
         self.area.insert(END, arg)
 
@@ -94,27 +102,125 @@ class ApplicationWindow():
             
         elif widgetSelected == self.CloseButton:
             self.frame.quit
-            
 
+    def ShowList(self):
+        FoodStructure = [[] for i in range(3)]
+        global ListIndex
+        global listWindowIndex
+        listWindowIndex = Tk()
+        ListIndex = ListWindow(listWindowIndex)
+        InsertItems()
+        
+#############################  Food List Window  ##################################            
+class ListWindow():
+    def __init__(self, master):
+        self.master = master
+        self.frame = Frame(self.master)
+        self.master.bind('<Button-1>', self.click_button)
+        self.initUI()
+
+    def initUI(self):
+        self.master.title("List")
+        self.style = Style()
+        self.style.theme_use("default")
+        self.frame.grid(row = 0, column = 0, sticky=N+E+S+W)
+
+        self.GoodFrame = Frame(self.master)
+        self.GoodFrame.grid(row=0, column=0, rowspan=10, columnspan=6)
+        self.GoodFrame.grid_rowconfigure(0, weight=1)
+        self.GoodFrame.grid_columnconfigure(0, weight=1)
+        self.GoodScrollbar = Scrollbar(self.GoodFrame, orient=VERTICAL)
+        self.GoodLabel = Label(self.GoodFrame, text="Good")
+        self.GoodLabel.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+        self.GoodList = Listbox(self.GoodFrame, selectmode=SINGLE, yscrollcommand=self.GoodScrollbar, height=10, width=20)      
+        self.GoodList.grid(row=1, column=0, rowspan=10, columnspan=5, padx=5, pady=5)
+        self.GoodScrollbar.config(command=self.Goodyview)
+        self.GoodScrollbar.grid(row=1, column=6, rowspan=10, sticky=N+S)
+        
+        self.OutSoonFrame = Frame(self.master)
+        self.OutSoonFrame.grid(row=0, column=6, rowspan=10, columnspan=6)
+        self.OutSoonFrame.grid_rowconfigure(0, weight=1)
+        self.OutSoonFrame.grid_columnconfigure(0, weight=1)
+        self.OutSoonScrollbar = Scrollbar(self.OutSoonFrame, orient=VERTICAL)
+        self.OutSoonLabel = Label(self.OutSoonFrame, text="Out Soon")
+        self.OutSoonLabel.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+        self.OutSoonList = Listbox(self.OutSoonFrame, selectmode=SINGLE, yscrollcommand=self.OutSoonScrollbar, height=10, width=20)
+        self.OutSoonList.grid(row=1, column=0, rowspan=10, columnspan=5, padx=5, pady=5)
+        self.OutSoonScrollbar.config(command=self.OutSoonyview)
+        self.OutSoonScrollbar.grid(row=1, column=6, rowspan=10, sticky=N+S)
+        
+        self.BadFrame = Frame(self.master)
+        self.BadFrame.grid(row=0, column=12, rowspan=10, columnspan=6)
+        self.BadFrame.grid_rowconfigure(0, weight=1)
+        self.BadFrame.grid_columnconfigure(0, weight=1) 
+        self.BadScrollbar = Scrollbar(self.BadFrame, orient=VERTICAL)        
+        self.BadLabel = Label(self.BadFrame, text="Spoiled")
+        self.BadLabel.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+        self.BadList = Listbox(self.BadFrame, selectmode=SINGLE, yscrollcommand=self.BadScrollbar, height=10, width=20)
+        self.BadList.grid(row=1, column=0, rowspan=10, columnspan=5, padx=5, pady=5)
+        self.BadScrollbar.config(command=self.Badyview)
+        self.BadScrollbar.grid(row=1, column=6, rowspan=10, sticky=N+S)
+
+        self.CloseFrame = Frame(self.master)
+        self.CloseFrame.grid(row=11, column=0, columnspan=20) 
+        self.CloseButton = Button(self.CloseFrame, text="Close")
+        self.CloseButton.grid(row=0, column=20, padx=5, pady=5, sticky=W+S)
+
+#############################  Functions for Food List Window  ##################################
+    def Goodyview(self, *args):
+        apply(self.GoodList.yview, args)
+    def OutSoonyview(self, *args):
+        apply(self.OutSoonList.yview, args)
+    def Badyview(self, *args):
+        apply(self.BadList.yview, args)
+
+    def InsertFoodItem(self, arg, listNumber):
+        if listNumber == 0:
+            self.GoodList.insert(END, arg)
+        elif listNumber == 1:
+            self.OutSoonList.insert(END, arg)
+        elif listNumber == 2:
+            self.BadList.insert(END, arg)
+        else:
+            print str(listNumber)+" <-- Not found"
+            
+    def click_button(self, event):
+        widgetSelected = event.widget
+        if widgetSelected == self.CloseButton:
+            print "you are here"
+            listWindowIndex.destroy()
+
+#############################  Other Functions  ##################################            
+
+def InsertItems():
+    FoodStructure = db.LoadFoodList()
+    WhichList = 0
+    for cols in FoodStructure:
+        for FoodString in cols:
+            ListIndex.InsertFoodItem(FoodString, WhichList)
+        WhichList = WhichList + 1
             
 def ListItem(PutMessageString):
     windowVariable.InsertListItem(PutMessageString) 
         
-def helpWindow():
-    print "You made it to the Help Window"
-
-def activate():
-    print "You are now activated"
+def CloseListWindow():
+    ListWindow.quit()
 
 def InitializeMainWindow(windowIndex):
     global windowVariable
-    windowIndex.geometry('368x355+50+50')
+##    windowIndex.geometry('368x355+50+50')
     windowVariable = ApplicationWindow(windowIndex)
 
-def AccessDatabase():
+def PrintAvailableFoods():
     length = len(foodInKitchen)
     for i in xrange(0,length):
-        ListItem(foodInKitchen[i].itemName)
+        if len(foodInKitchen[i].itemName) < 30:
+            StringTemp = '{:<30}'.format(foodInKitchen[i].itemName)
+            String = StringTemp + " (" + foodInKitchen[i].itemQuantity + ")"
+        else:
+            String = foodInKitchen[i].itemName + "       (" + foodInKitchen[i].itemQuantity + ")"
+            
+        ListItem(String)
 
 def CalculateSpoilTimes():
     print "You are now calculating the times."
@@ -130,7 +236,7 @@ def CalculateSpoilTimes():
            
         TimeNow = db.TimeClass(hourNow, minuteNow, monthNow, dayNow, yearNow)
         length = len(foodInKitchen)
-##        db.CheckIfItemsAreExpired(TimeNow, foodInKitchen)
+        db.CheckIfItemsAreExpired(TimeNow, foodInKitchen)
         
 def InsertListData(listIndex):
     print listIndex
@@ -138,7 +244,7 @@ def InsertListData(listIndex):
 index = Tk()
 InitializeMainWindow(index)
 foodInKitchen = db.ReadInDatabase()
-AccessDatabase()
+PrintAvailableFoods()
 index.after(10, CalculateSpoilTimes)
 index.mainloop()
 
--- Database.py
+++ Database.py
@@ -1,6 +1,7 @@
 import os
 import ConfigParser
 import re
+import time
 
 kitchenDatabaseFilename = "\\Kitchen Database\\KitchenDatabase.ini"
 
@@ -24,6 +25,8 @@ class DatabaseContents:
                 self.datePurchased = data[i][1]
             elif data[i][0] == "itemshelflife":
                 self.itemShelfLife = data[i][1]
+            elif data[i][0] == "quantity":
+                self.itemQuantity = data[i][1]
             else:
                 print "The data not found: "+data[i][0]
 
@@ -35,7 +38,6 @@ def ReadInDatabase():
     kitchenDatabaseIndex.read(kitchenDatabaseFilepath)
 
     kitchenSections = kitchenDatabaseIndex.sections()
-    i = 0
     for section in kitchenSections:
         options = kitchenDatabaseIndex.items(section)
         KitchenStructure.append(DatabaseContents(section, options))
@@ -43,34 +45,155 @@ def ReadInDatabase():
     return KitchenStructure
 
 def CheckIfItemsAreExpired(Current, foodStructure):
+    goodCounter = 0
+    badCounter = 0
+    outSoonCounter = 0
+    errorCounter = 0
+    
     timeStamp = Current.hour+":"+Current.minute+" "+Current.month+"."+Current.day+"."+str(Current.year)
 
     FoodListIndex = ConfigParser.ConfigParser()
     FoodListIndex.add_section("TimeStamp")
+    FoodListIndex.add_section("Good Food")
+    FoodListIndex.add_section("Out Soon Food")
+    FoodListIndex.add_section("Spoiled Food")
+    FoodListIndex.add_section("Error")
     FoodListIndex.set("TimeStamp", "TimeAdded", timeStamp)
+
+##    FoodListIndex.add_section("Good Food")
+##    for i in range(0, 10):
+##        string1 = "GoodFood"+str(i)
+##        FoodListIndex.set("Good Food", string1, string1)
+##        
+##    FoodListIndex.add_section("Out Soon Food")
+##    for i in range(0, 10):
+##        string2 = "OutSoonFood"+str(i)
+##        FoodListIndex.set("Out Soon Food", string2, string2)
+##        
+##    FoodListIndex.add_section("Spoiled Food")
+##    for i in range(0, 10):
+##        string3 = "SpoiledFood"+str(i)
+##        FoodListIndex.set("Spoiled Food", string3, string3)
 
     length = len(foodStructure)
     for i in range(0, length):
         timeTilExpiresString = foodStructure[i].itemShelfLife
         if "day" in timeTilExpiresString or "d" in timeTilExpiresString:
-            print "Found days"
             found = re.findall('\d+', timeTilExpiresString)
             number = int(found[0])*24
         elif "hour" in timeTilExpiresString or "hr" in timeTilExpiresString or "h" in timeTilExpiresString:
-            print "Found hours"
             found = re.findall('\d+', timeTilExpiresString)
             number = int(found[0])
         elif "year" in timeTilExpiresString or "yr" in timeTilExpiresString or "y" in timeTilExpiresString:
-            print "Found years"
             found = re.findall('\d+', timeTilExpiresString)
             number = int(found[0])*24*365
         else:
             number = 0
-        print "The number is: "+str(number)
 
-        
+        foodDecision = Result(Current, foodStructure[i].datePurchased, number)
 
-    FoodList = open("FoodList.ini", "w+")
+        if foodDecision == 1:
+            print "You are in number 1"
+            goodStringIndex = "GoodString" + str(goodCounter)
+            FoodListIndex.set("Good Food", goodStringIndex, foodStructure[i].itemName)
+            goodCounter = goodCounter + 1
+        elif foodDecision == 2:
+            outSoonStringIndex = "OutSoonString" + str(outSoonCounter)
+            FoodListIndex.set("Out Soon Food", outSoonStringIndex, foodStructure[i].itemName)
+            outSoonCounter = outSoonCounter + 1 
+        elif foodDecision == 3:
+            badStringIndex = "BadString" + str(badCounter)
+            FoodListIndex.set("Spoiled Food", badStringIndex, foodStructure[i].itemName)
+            badCounter = badCounter + 1
+        else:
+            print "There was an error"
+            errorStringIndex = "ErrorString" + str(errorCounter)
+            FoodListIndex.set("Error", errorStringIndex, foodStructure[i].itemName)
+            errorCounter = errorCounter + 1
+            
+            
+
+    FoodList = open("Kitchen Database\\FoodList.ini", "w+")
     FoodListIndex.write(FoodList)
     FoodList.close()
+    time.sleep(2)
+
+def Result(Time, purchaseDate, timeTilExpires):
+    foodDecision = 1
+##    ThirtyOneDays = [01, 03, 05, 07, 08, 10, 12]
+    exception = 0
+    print "You made it inside the results function"
     
+    print purchaseDate
+##    foundTimeNow = re.search("(.*?):(.*?) (.*?)\.(.*?)\.(.*?)$", purchaseDate)    
+##    if foundTimeNow:
+##        hour = int(foundTimeNow.group(1))
+##        minute = int(foundTimeNow.group(2))
+##        month = int(foundTimeNow.group(3))
+##        day = int(foundTimeNow.group(4))
+##        year = int(foundTimeNow.group(5))
+##
+####        print "hour "+str(hour)
+####        print "minute "+str(minute)
+####        print "month "+str(month)
+####        print "day "+str(day)
+####        print "year "+str(year)
+##
+##        if Time.yearNow > year:
+##            elapsedYears = Time.yearNow - year
+##            if elapsedYears > 1:
+##                if Time.monthNow < month:
+##                    elapsedMonths = (Time.monthNow + 12) - month
+##                else:
+##                    elapsedMonths = (Time.monthNow) - month
+##                    elapsedMonths = elapsedMonths + (elapsedYears * 12)
+##                
+##        
+##
+##
+##        elapsedHours = Time.hourNow - hour
+##        elapsedMinutes = Time.minuteNow - minute
+##        elapsedMonths = Time.monthNow - month
+##        elapsedDays = (Time.dayNow + exception) - day
+##        elapsedYears = Time.yearNow - year
+        
+
+    
+    
+
+
+
+
+
+    return foodDecision 
+
+
+def LoadFoodList():
+    currentDirectory = os.getcwd()
+    FoodFilepath = currentDirectory+"\\Kitchen Database\\FoodList.ini"
+    FoodListIndex = ConfigParser.ConfigParser()
+    FoodListIndex.read(FoodFilepath)
+    
+    FoodListStructure = [[] for i in range(3)]
+    FoodSections = FoodListIndex.sections()
+    
+    for section in FoodSections:
+        if section != "TimeStamp" and section != "Error":
+            print "You made after the section check"
+            options = FoodListIndex.items(section)
+            if section == "Good Food":
+                listNumber = 0
+            elif section == "Out Soon Food":
+                listNumber = 1
+            elif section == "Spoiled Food":
+                listNumber = 2
+            else:
+                print "Error!!"
+
+            for option in options:
+##                print option[1]
+                FoodListStructure[listNumber].append(option[1])
+##                print FoodListStructure[listNumber]
+    return FoodListStructure
+                
+            
