7a5d5c366d60303f14420fd37f0ec5220859fdbb
John Howland <chase.howland1@gmail.com>
2015-04-05 20:28:32 -0400

    Added Background
    Added background to the main window. Still need to add in the smaller
    details

--- ApplicationWindow.py
+++ ApplicationWindow.py
@@ -2,83 +2,76 @@ from Tkinter import *
 from ttk import *
 import Database as db
 import string
+import os
 from time import strftime
 from PIL import Image, ImageTk
 
 itemFilepath = "C:\\Projects\\Testing Location\\Smart Kitchen\\Product Image\\00000000.gif"
-
+MainScreenBackGround = "C:\\Projects\\Testing Location\\Smart Kitchen\\Product Image\\MainScreenBackGround.png"
+NewGroceriesBackGround = "C:\\Projects\\Testing Location\\Smart Kitchen\\Product Image\\GroceryShoppingBackGround.gif"
 
 #############################  Main Application Window  ##################################
-class ApplicationWindow():
-    def __init__(self, master):
-        self.master = master
-##        self.master.resizable(FALSE, FALSE)
-        self.frame = Frame(self.master)
-        self.master.bind('<Button-1>', self.click_button)
-##        self.master.curIndex = None
-        
-##        self.textFrame = Frame(self.master)
-        self.initUI()
-        
-    def initUI(self):
-        self.master.title("Windows")
-        self.style = Style()
-        self.style.theme_use("default")
-        self.frame.grid(row = 0, column = 0, sticky=N+E+S+W)
-            
-##        Button(master, text="Button {0}".format(c)).grid(row=6,column=c,sticky=E+W)
-##        self.frame.columnconfigure(1, pad=5, weight=10)
-##        self.frame.columnconfigure(8, pad=7)
-##        self.frame.rowconfigure(1, pad=5, weight=10)
-##        self.frame.rowconfigure(12, pad=7)
-        
-        self.lbl = Label(self.frame, text="Windows")
-        self.lbl.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+def InitializeMainWindowFeatures(root, mainSelf):
+    
+    mainSelf.lbl = Label(root, text="Foods")
+    mainSelf.lbl.grid(row=0, column=0, sticky=W)
+    mainSelf.area = Listbox(root, selectmode=SINGLE, height=17, width=18)
+    mainSelf.area.grid(row=1, column=0, rowspan=10, columnspan=2, padx=5, sticky=W)
+    
+    mainSelf.itemName = Entry(root, width = 18)
+    mainSelf.itemName.grid(row=1, column=3, padx=5, sticky = W)
+    mainSelf.itemName_LABEL = Label(root, text = "Item Name")
+    mainSelf.itemName_LABEL.grid(row=0, column=3, sticky=W)
 
-        self.area = Listbox(self.frame, selectmode=SINGLE, height=17, width=25)
-        self.area.grid(row=1, column=0, rowspan=10, columnspan=2, padx=5, pady=5)
-        
-        self.itemName = Entry(self.frame, width = 18)
-        self.itemName.grid(row=1, column=3, padx=5, sticky = W)
-        self.itemName_LABEL = Label(self.frame, text = "Item Name")
-        self.itemName_LABEL.grid(row=0, column=3, pady=2, padx=5, sticky=W)
+    mainSelf.itemDatePurchased = Entry(root, width = 15)
+    mainSelf.itemDatePurchased.grid(row=4, column=3, padx=5, sticky=W)
+    mainSelf.itemDatePurchased_LABEL = Label(root, text = "Date Purchased")
+    mainSelf.itemDatePurchased_LABEL.grid(row=3, column=3, sticky=W)
 
-        self.itemDatePurchased = Entry(self.frame, width = 15)
-        self.itemDatePurchased.grid(row=4, column=3, padx=5, sticky=W)
-        self.itemDatePurchased_LABEL = Label(self.frame, text = "Date Purchased")
-        self.itemDatePurchased_LABEL.grid(row=3, column=3, pady=2, padx=5, sticky=W)
+    mainSelf.itemSelfLifeTime = Entry(root, width = 15)
+    mainSelf.itemSelfLifeTime.grid(row=4, column=4, padx=5, sticky=W)
+    mainSelf.itemSelfLifeTime_LABEL = Label(root, text = "Expire Time")
+    mainSelf.itemSelfLifeTime_LABEL.grid(row=3, column=4, sticky=W)
 
-        self.itemSelfLifeTime = Entry(self.frame, width = 15)
-        self.itemSelfLifeTime.grid(row=4, column=4, padx=5, sticky=W)
-        self.itemSelfLifeTime_LABEL = Label(self.frame, text = "Expire Time")
-        self.itemSelfLifeTime_LABEL.grid(row=3, column=4, pady=2, padx=5, sticky=W)
+    mainSelf.itemQuantity = Entry(root, width = 5)
+    mainSelf.itemQuantity.grid(row=1, column=4, padx=5, sticky=W)
+    mainSelf.itemQuantity_LABEL = Label(root, text = "Qantity")
+    mainSelf.itemQuantity_LABEL.grid(row=0, column=4, sticky=W)
 
-        itemFilepath = "C:\\Projects\\Testing Location\\Smart Kitchen\\Product Image\\NoPictureAvailable.gif"
-        self.photo = PhotoImage(file = itemFilepath)
-        self.imageCanvas = Canvas(self.frame, width=200, height=200)
-        self.imageCanvas.grid(row=5, column=3, rowspan=6, columnspan=3, padx=5, pady=5)
-        self.imageCanvas.create_image(100,100, image = self.photo)
+    itemFilepath = "C:\\Projects\\Testing Location\\Smart Kitchen\\Product Image\\NoPictureAvailable.gif"
+    photo = PhotoImage(file = itemFilepath)
+    mainSelf.imageCanvas = Canvas(root, width=50, height=180)
+    mainSelf.imageCanvas.grid(row=5, column=3, rowspan=5, columnspan=2, padx=5, pady=5, sticky=W+E+N+S)
+    mainSelf.imageCanvas.create_image(0,0, image = photo)
 
-##        self.photo = PhotoImage(Image.open(itemFilepath))
-##        self.label = Label(image=self.photo)
-##        self.label.image = photo # keep a reference!
-##        label.pack()
-
-##        itemImage = Image.open(itemFilepath)
-##        self.itemPicture = PhotoImage(image=itemImage)
-##        self.itemPicture.grid(row=3, column=3, rowspan=7, columnspan=4, padx=5, pady=5)
-
-        self.CloseButton = Button(self.frame, text="Close", command = self.frame.quit)
-        self.CloseButton.grid(row=12, column=4, padx=5, pady = 5, sticky=E+S)
-        
+    mainSelf.CloseButton = Button(root, text="Close", command = root.quit)
+    mainSelf.CloseButton.grid(row=12, column=4, padx=5, pady = 5, sticky=E+S)
+    
 ##        self.hbtn = Button(self.frame, text="Help", command = helpWindow)
 ##        self.hbtn.grid(row=5, column=0, padx=5)
-##
-##        self.obtn = Button(self.frame, text="OK", command = test)
-##        self.obtn.grid(row=5, column=3)
 
-        self.ListButton = Button(self.frame, text="List", command = self.ShowList)
-        self.ListButton.grid(row=12, column=0, padx=5, pady = 5, sticky=W+S)
+    mainSelf.obtn = Button(root, text="Went Shopping!", command = MainWindow.InsertNewItems)
+    mainSelf.obtn.grid(row=0, column=5, sticky=N+S)
+
+    mainSelf.ListButton = Button(root, text="List", command = MainWindow.ShowList)
+    mainSelf.ListButton.grid(row=12, column=0, padx=5, pady = 5, sticky=W+S)
+
+
+
+class ApplicationWindow(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.master = master
+##        self.master.resizable(FALSE, FALSE)
+        self.master.bind('<Button-1>', self.click_button)
+        self.initUI()        
+        
+    def initUI(self):
+        self.master.title("Smart Kitchen 1.00")
+        self.grid(sticky = N+S+E+W)
+
+    def GetSelfMaster(self):
+        return self
 
 #############################  Functions for Main Window  ##################################
 
@@ -91,17 +84,21 @@ class ApplicationWindow():
             self.itemName.delete(0, END)
             self.itemDatePurchased.delete(0, END)
             self.itemSelfLifeTime.delete(0, END)
+            self.itemQuantity.delete(0, END)
             listSelection = self.area.curselection()
             self.itemName.insert(0, foodInKitchen[listSelection[0]].itemName)
             self.itemDatePurchased.insert(0, foodInKitchen[listSelection[0]].datePurchased)
             self.itemSelfLifeTime.insert(0, foodInKitchen[listSelection[0]].itemShelfLife)
+            self.itemQuantity.insert(0, foodInKitchen[listSelection[0]].itemQuantity)
 
             itemFilepath = "C:\\Projects\\Testing Location\\Smart Kitchen\\Product Image\\"+foodInKitchen[listSelection[0]].itemVIN+".gif"
+            if not os.path.isfile(itemFilepath):
+                itemFilepath = "C:\\Projects\\Testing Location\\Smart Kitchen\\Product Image\\NoPictureAvailable.gif"
             self.photo = PhotoImage(file = itemFilepath)
-            self.imageCanvas.create_image(100,100, image = self.photo)
+            self.imageCanvas.create_image(100, 80, image = self.photo)
             
         elif widgetSelected == self.CloseButton:
-            self.frame.quit
+            self.quit
 
     def ShowList(self):
         FoodStructure = [[] for i in range(3)]
@@ -110,6 +107,14 @@ class ApplicationWindow():
         listWindowIndex = Tk()
         ListIndex = ListWindow(listWindowIndex)
         InsertItems()
+
+    def InsertNewItems(self):
+        global NewItemsIndex
+        global NewItemsWindowIndex
+        NewItemsWindowIndex = Tk()
+        NewItemsIndex = NewItemsWindow(NewItemsWindowIndex)
+        PopulateExpireTimesWindow()
+        
         
 #############################  Food List Window  ##################################            
 class ListWindow():
@@ -190,61 +195,163 @@ class ListWindow():
             print "you are here"
             listWindowIndex.destroy()
 
-#############################  Other Functions  ##################################            
+#############################  Insert New Items  #################################
+class NewItemsWindow():
+    def __init__(self, master):
+        self.master = master
+        self.frame = Frame(self.master)
+        self.master.bind('<Button-1>', self.click_button)
+        self.initUI()
 
-def InsertItems():
-    FoodStructure = db.LoadFoodList()
-    WhichList = 0
-    for cols in FoodStructure:
-        for FoodString in cols:
-            ListIndex.InsertFoodItem(FoodString, WhichList)
-        WhichList = WhichList + 1
-            
-def ListItem(PutMessageString):
-    windowVariable.InsertListItem(PutMessageString) 
+    def initUI(self):
+        self.master.title("New Groceries!")
+        self.style = Style()
+        self.style.theme_use("default")
+        self.frame.grid(row = 0, column = 0, sticky=N+E+S+W)
+
+        self.itemName = Entry(self.frame, width = 18)
+        self.itemName.grid(row=1, column=1, padx=5, sticky = W)
+        self.itemName_LABEL = Label(self.frame, text = "Item Name")
+        self.itemName_LABEL.grid(row=0, column=1, pady=2, padx=5, sticky=W)
+
+        self.itemQuantity = Entry(self.frame, width = 15)
+        self.itemQuantity.grid(row=1, column=3, padx=5, sticky=W)
+        self.itemQuantity_LABEL = Label(self.frame, text = "Quantity")
+        self.itemQuantity_LABEL.grid(row=0, column=3, pady=2, padx=5, sticky=W)
+
+        self.ExpireTimeFrame = Frame(self.master)
+        self.ExpireTimeFrame.grid(row=3, column=0, rowspan=10, columnspan=3, sticky=N+S+E+W)
+        self.ExpireTimeFrame.grid_rowconfigure(0, weight=1)
+        self.ExpireTimeFrame.grid_columnconfigure(0, weight=1)
+        self.ExpireTimeLabel = Label(self.ExpireTimeFrame, text="Expire Time")
+        self.ExpireTimeLabel.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+        self.ExpireTimeScrollbar = Scrollbar(self.ExpireTimeFrame, orient=VERTICAL)
+        self.ExpireTimeList = Listbox(self.ExpireTimeFrame, selectmode=SINGLE, yscrollcommand=self.ExpireTimeScrollbar, height=10, width=20)
+        self.ExpireTimeList.grid(row=1, column=0, rowspan=5, columnspan=5, padx=5, sticky=W+N)
+        self.ExpireTimeScrollbar.config(command=self.ExpireTimeyview)
+        self.ExpireTimeScrollbar.grid(row=1, column=1, rowspan=5, sticky=N+S) 
+ 
+        self.CloseButton = Button(self.ExpireTimeFrame, text="Close")
+        self.CloseButton.grid(row=6, column=7, padx=5, pady=5, sticky=W+S)
+
+        self.InsertButton = Button(self.ExpireTimeFrame, text="Insert", )
+        self.InsertButton.grid(row=6, column=0, padx=5, pady=5, sticky=E+S)
+        self.InsertButton.grid_remove()
         
-def CloseListWindow():
-    ListWindow.quit()
+#############################  Functions for Insert New Items  ##################################
+    def click_button(self, event):
+        widgetSelected = event.widget
+        if widgetSelected == self.CloseButton:
+            NewItemsWindowIndex.destroy()
+        if widgetSelected == self.InsertButton:
+            print "You have inserted this item!"
+            self.InsertEnteredNewGrocery()
+        if widgetSelected == self.ExpireTimeList:
+            self.InsertButton.grid()
 
-def InitializeMainWindow(windowIndex):
-    global windowVariable
-##    windowIndex.geometry('368x355+50+50')
-    windowVariable = ApplicationWindow(windowIndex)
+    def InsertEnteredNewGrocery(self):
+        err = 0
+        itemNameString = self.itemName.get()
+        itemQuantityString = self.itemQuantity.get()
+        sel = self.ExpireTimeList.curselection()
+        itemExpireTimeString = db.ExpireTimeStringList[sel[0]]
+        if len(itemNameString) < 1 or len(itemQuantityString) < 1:
+            print "Error!!"
+        else:
+            err = db.InsertNewGroceryItem(itemNameString, itemQuantityString, itemExpireTimeString)
+            if err:
+                if err == -1:
+                    print "An error occured while inserting grocery"
+            else:
+                print "Item was inserted successfully"
+            
 
-def PrintAvailableFoods():
+    def ExpireTimeyview(self, *args):
+        apply(self.ExpireTimeList.yview, args)
+
+    def InsertExpireTimes(self, args):
+        self.ExpireTimeList.insert(END, args)
+        
+#############################  Error Message Window  #################################
+##class ErrorMessageWindow():
+##    def __init__(self, master):
+##        self.master = master
+##        self.frame = Frame(self.master)
+##        self.master.bind('<Button-1>', self.click_button)
+##        self.initUI()
+##
+##    def initUI(self):
+##        self.master.title("ERROR")
+##        self.style = Style()
+##        self.style.theme_use("default")
+##        self.frame.grid(row = 0, column = 0, sticky=N+E+S+W)
+
+
+#############################  Other Functions  ##################################
+##def InsertItems():
+##    FoodStructure = db.LoadFoodList()
+##    WhichList = 0
+##    for cols in FoodStructure:
+##        for FoodString in cols:
+##            ListIndex.InsertFoodItem(FoodString, WhichList)
+##        WhichList = WhichList + 1
+##
+##def PopulateExpireTimesWindow():
+##    for dayString in db.ExpireTimeStringList:
+##        NewItemsIndex.InsertExpireTimes(dayString)
+##            
+##def ListItem(PutMessageString):
+##    windowVariable.InsertListItem(PutMessageString) 
+##        
+##def CloseListWindow():
+##    ListWindow.quit()
+##
+##def InitializeMainWindow(windowIndex):
+##    global windowVariable
+####    windowIndex.geometry('368x355+50+50')
+##    windowVariable = ApplicationWindow(windowIndex)
+##
+def PrintAvailableFoods(root, mainSelf):
     length = len(foodInKitchen)
     for i in xrange(0,length):
-        if len(foodInKitchen[i].itemName) < 30:
-            StringTemp = '{:<30}'.format(foodInKitchen[i].itemName)
-            String = StringTemp + " (" + foodInKitchen[i].itemQuantity + ")"
-        else:
-            String = foodInKitchen[i].itemName + "       (" + foodInKitchen[i].itemQuantity + ")"
-            
-        ListItem(String)
+        String = foodInKitchen[i].itemName
+        mainSelf.area.insert(END, String)
 
-def CalculateSpoilTimes():
-    print "You are now calculating the times."
+##def CalculateSpoilTimes():
+##    print "You are now calculating the times."
+##
+##    timeString = strftime('%X %x %Z')
+##    foundTimeNow = re.search("(.*?):(.*?):[0-9][0-9] (.*?)/(.*?)/(.*?) .*", timeString)    
+##    if foundTimeNow:
+##        hourNow = foundTimeNow.group(1)
+##        minuteNow = foundTimeNow.group(2)
+##        monthNow = foundTimeNow.group(3)
+##        dayNow = foundTimeNow.group(4)
+##        yearNow = int(foundTimeNow.group(5)) + 2000
+##           
+##        TimeNow = db.TimeClass(hourNow, minuteNow, monthNow, dayNow, yearNow)
+##        length = len(foodInKitchen)
+##        db.CheckIfItemsAreExpired(TimeNow, foodInKitchen)
+##        
+##def InsertListData(listIndex):
+##    print listIndex
 
-    timeString = strftime('%X %x %Z')
-    foundTimeNow = re.search("(.*?):(.*?):[0-9][0-9] (.*?)/(.*?)/(.*?) .*", timeString)    
-    if foundTimeNow:
-        hourNow = foundTimeNow.group(1)
-        minuteNow = foundTimeNow.group(2)
-        monthNow = foundTimeNow.group(3)
-        dayNow = foundTimeNow.group(4)
-        yearNow = int(foundTimeNow.group(5)) + 2000
-           
-        TimeNow = db.TimeClass(hourNow, minuteNow, monthNow, dayNow, yearNow)
-        length = len(foodInKitchen)
-        db.CheckIfItemsAreExpired(TimeNow, foodInKitchen)
-        
-def InsertListData(listIndex):
-    print listIndex
 
-index = Tk()
-InitializeMainWindow(index)
+root = Tk()
+root.geometry("500x350+300+300")
+global MainWindow
+MainWindow = ApplicationWindow(root)
+
+im = Image.open(MainScreenBackGround)
+tkimage = ImageTk.PhotoImage(im)
+myvar=Label(root, image = tkimage)
+myvar.place(x=0, y=0, relwidth=1, relheight=1)
+mainSelf = MainWindow.GetSelfMaster()
+print mainSelf
+InitializeMainWindowFeatures(root, mainSelf)
+
 foodInKitchen = db.ReadInDatabase()
-PrintAvailableFoods()
-index.after(10, CalculateSpoilTimes)
-index.mainloop()
+PrintAvailableFoods(root, mainSelf)
+##index.after(10, CalculateSpoilTimes)
+root.mainloop()
 
--- Database.py
+++ Database.py
@@ -5,6 +5,18 @@ import time
 
 kitchenDatabaseFilename = "\\Kitchen Database\\KitchenDatabase.ini"
 
+ExpireTimeStringList = []
+ExpireTimeStringList = ['1 day','2 days','3 days',
+                        '4 days','5 days','6 days',
+                        '7 days','8 days','9 days',
+                        '10 days','11 days','12 days',
+                        '13 days','14 days','15 days',
+                        '16 days','17 days','18 days',
+                        '19 days','20 days','21 days',
+                        '22 days','23 days','24 days',
+                        '25 days','26 days','27 days',
+                        '28 days','29 days','30 days']
+
 class TimeClass:    
     def __init__(self, currentHour, currentMinute, currentMonth, currentDay, currentYear):
         self.hour = currentHour
@@ -195,5 +207,11 @@ def LoadFoodList():
                 FoodListStructure[listNumber].append(option[1])
 ##                print FoodListStructure[listNumber]
     return FoodListStructure
+
+def InsertNewGroceryItem(name, quantity, expireTime):
+    err = 0
+    print "You made it into insert function in database file!"
+
+    return err
                 
             
